0b7d3ab74e70790004e10de0eee3df3088f58a1c
patmallee <patmallee@gmail.com>
2015-09-30 19:23:21 -0400

    finished product - actually
    Actually finished the project this time. Program now creates the screen
    based on user input and each pixel darkens upon subsequent mouseovers

--- EaS.js
+++ EaS.js
@@ -1,27 +1,30 @@
 
 $(document).ready(function() {
-	reset()
+	reset();
 });
+
+/* everything working fine until we use the reset button */
 
 function reset() {
 	var screenSize = prompt("How large would you like your new screen to be?", "###");
-	$('#screen').empty();	
+	$('.screen').empty();	
 	
 	var gridSize = screenSize * screenSize;
-	if (screenSize != null) {
-		for (i = 0; i < gridSize; i++) {
-			$('#screen').append("<div class='pixel' opacity='1'></div>");
-		}	
-	}
+	var pixelSize = (600 / screenSize) + 'px';
 
-	var pixelSize = (500 / screenSize) + 'px';
+	for (i = 0; i < gridSize; i++) {
+		$('.screen').append("<div class='pixel'></div>");
+	}
+	$('.screen').append("<div class='break'></div>");
+
 	$('.pixel').css('width', pixelSize);
 	$('.pixel').css('height', pixelSize);
 
-	$('.pixel').hover(function() {
-		var currentOpacity = $(this).css('opacity');
-		if(currentOpacity < 1) {
-			$(this).css('opacity', currentOpacity + 0.1);
+	$('.pixel').mouseover(function() {
+		var currentOpacity = $(this).css('opacity') * 10;
+		if(currentOpacity < 10) {
+			currentOpacity = (currentOpacity + 1) / 10
+			$(this).css('opacity', currentOpacity);
 		}
 	});
 }
